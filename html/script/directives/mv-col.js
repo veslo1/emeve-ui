4cb00bb174d7b06393919bf6a8c9e27bdad29173
Marcus Vinicius <marcusvy@gmail.com>
2014-09-16 16:34:35 -0400

    Grid Finalizada

--- html/script/directives/mv-col.js
+++ html/script/directives/mv-col.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('EmeveUiApp')
-    .directive('mvCol', function () {
+    .directive('mvCol', function ($parse, $compile) {
         return {
             restrict: 'C',
             template: '',
@@ -12,10 +12,23 @@ angular.module('EmeveUiApp')
                 pull: '@'
             },
             controller: function ($scope, $element, $attrs) {
+                $scope.layoutObj = {};
 
+                $scope.produceClass = function (type, size) {
+                    var className = type + '-' + size;
+                    $element.addClass(className);
+                };
             },
             link: function (scope, element, attrs, mvColCtrl, transclude) {
-                element.append(transclude());
+                scope.layout = angular.isDefined(scope.layout) ? $parse(scope.layout)(scope) : scope.layoutObj;
+
+                transclude(scope.$parent, function (clone, scope) {
+                    element.append(clone);
+                });
+
+                angular.forEach(scope.layout, function (value, key) {
+                    scope.produceClass(key, value);
+                });
             }
         };
     })
--- html/script/directives/mv-row.js
+++ html/script/directives/mv-row.js
@@ -11,15 +11,15 @@ angular.module('EmeveUiApp')
             transclude: true,
             link: function (scope, element, attrs, mvRowCtrl, transclude) {
                 scope.layoutFill = (angular.isDefined(scope.layoutFill)) ? JSON.parse(scope.layoutFill) : false
+                element.append(transclude());
 
                 if (scope.layoutFill) {
-                    console.log(element)
-                    var max = element.children()[0].offsetHeight;
-                    angular.forEach(element.children()[0].children, function (value) {
+                    var max = element[0].offsetHeight;
+                    angular.forEach(element[0].children, function (value) {
                         angular.element(value).css('height',max+'px');
                     });
                 }
-                element.append(transclude());
+
             }
         };
     });
