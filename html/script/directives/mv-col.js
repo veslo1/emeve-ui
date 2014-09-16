3ec3ce5fa9748b74719b57a6d3bfac573a0e08f1
Marcus Vinicius <marcusvy@gmail.com>
2014-09-16 15:02:31 -0400

    Fix Transclude

--- html/script/directives/mv-col.js
+++ html/script/directives/mv-col.js
@@ -1,3 +1,21 @@
-/**
- * Created by Marcus on 16/09/2014.
- */
+'use strict';
+
+angular.module('EmeveUiApp')
+    .directive('mvCol', function () {
+        return {
+            restrict: 'C',
+            template: '',
+            transclude: true,
+            scope: {
+                layout: '@',
+                push: '@',
+                pull: '@'
+            },
+            controller: function ($scope, $element, $attrs) {
+
+            },
+            link: function (scope, element, attrs, mvColCtrl, transclude) {
+                element.append(transclude());
+            }
+        };
+    })
--- html/script/directives/mv-row.js
+++ html/script/directives/mv-row.js
@@ -3,39 +3,23 @@
 angular.module('EmeveUiApp')
     .directive('mvRow', function () {
         return {
-            restrict: 'E',
-            template: '<div class="mv-row" ng-transclude></div>',
+            restrict: 'C',
+            template: '',
             scope: {
                 layoutFill: '@'
             },
             transclude: true,
-            link: function (scope, element, attrs) {
+            link: function (scope, element, attrs, mvRowCtrl, transclude) {
                 scope.layoutFill = (angular.isDefined(scope.layoutFill)) ? JSON.parse(scope.layoutFill) : false
+
                 if (scope.layoutFill) {
+                    console.log(element)
                     var max = element.children()[0].offsetHeight;
                     angular.forEach(element.children()[0].children, function (value) {
                         angular.element(value).css('height',max+'px');
                     });
                 }
+                element.append(transclude());
             }
         };
     });
-/*
- app.directive('mvCol', function ($scope) {
- return {
- restrict: 'C',
- template: '<div></div>',
- transclude: true,
- scope: {
- layout: '@',
- push: '@',
- pull: '@'
- },
- controller: function($scope,$element,$attrs){
-
- },
- link: function(scope, element, attrs){
-
- }
- };
- })*/
