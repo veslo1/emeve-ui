f6278a3149e4384089b44175d2063f5efd416b81
Marcus Vinícius <marcusvy@gmail.com>
2014-10-27 14:51:33 -0400

    Update: mv-dropdown: remove unecessary variables

--- html/script/directives/mv-dropdown.js
+++ html/script/directives/mv-dropdown.js
@@ -22,15 +22,13 @@ angular.module('EmeveUiApp')
       },
       link: function (scope, element, attrs, DropdownCtrl) {
         var btn = angular.element(element.children()[0]);
-        var menu = angular.element(element.children()[1]);
-
-        //adicionar caret
-        btn.append(DropdownCtrl.addCaret());
-
         var doOpen = function ($event) {
           $event.preventDefault();
           DropdownCtrl.open();
         };
+
+        //adicionar caret
+        btn.append(DropdownCtrl.addCaret());
 
         //WAI ARIA
         element.attr({ 'aria-haspopup': true, 'aria-expanded': false });
@@ -50,42 +48,3 @@ angular.module('EmeveUiApp')
       }
     };
   });
-
-
-//atual
-//angular.module('EmeveUiApp')
-//  .directive('mvDropdown', function () {
-//    return {
-//      restrict: 'C',
-//      controller: function ($scope, $element, $attrs) {
-//        $scope.isOpen = false;
-//
-//        this.open = function ($event) {
-//          $scope.isOpen = !$scope.isOpen;
-//          $scope.$apply(function () {
-//            $element.toggleClass('open', $scope.isOpen);
-//          });
-//          return $scope.isOpen;
-//        };
-//      },
-//      link: function (scope, element, attrs, DropdowCtrl) {
-//        var btn = angular.element(element.children()[0]);
-//
-//        element.addClass('dropdown')
-//
-//        var doOpen = function ($event) {
-//          $event.preventDefault();
-//          DropdowCtrl.open();
-//        };
-//
-//        //Abertura do menu
-//        btn.bind('click', doOpen);
-//        //element.bind('focusout',doOpen);
-//
-//        scope.$on('$destroy', function() {
-//          btn.unbind('click', doOpen);
-//          element.unbind('focusout', doOpen);
-//        });
-//      }
-//    };
-//  });
