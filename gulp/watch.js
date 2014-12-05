55dc29563b2cf67caab28faf8d7c80cb7644c343
Marcus Vinicius <marcusvy@gmail.com>
2014-12-05 19:36:39 -0400

    Gulp: Update Watch for demo section

--- gulp/watch.js
+++ gulp/watch.js
@@ -7,14 +7,24 @@ var gulp = require('gulp');
 
 var appSettings = require('./config.json').appSettings;
 var dirDev = appSettings.directory.dev; //app
+var dirDemo = appSettings.directory.demo; //app
 
 gulp.task('watch', function () {
   gulp.watch('bower.json', ['bower']);
   gulp.watch('module/**/*.{php,phtml}', ['phpView']);
-  gulp.watch(dirDev + 'styles/**/*.css', ['styles']);
-  gulp.watch(dirDev + 'styles/**/*.scss', ['sass']);
+  gulp.watch([
+    dirDev + 'styles/**/*.css',
+    dirDemo + 'styles/**/*.css'
+  ], ['styles']);
+  gulp.watch([
+    dirDev + 'styles/**/*.scss',
+    dirDemo + 'styles/**/*.scss'
+  ], ['sass']);
   //gulp.watch(dirDev + 'scripts/**/*.js', ['scripts','jsHint']);
-  gulp.watch(dirDev + 'scripts/**/*.js', ['scripts']);
-  gulp.watch(dirDev + "views/**/*.html",['ngDirectives']);
+  gulp.watch([
+    dirDev + 'scripts/**/*.js',
+    dirDemo + 'scripts/**/*.js'
+  ], ['scripts']);
+  gulp.watch(dirDev + "partial/directives/**/*.html",['ngDirectives']);
   gulp.watch(dirDev + "images/**/*.{gif,png,jpg}",['images']);
 });
