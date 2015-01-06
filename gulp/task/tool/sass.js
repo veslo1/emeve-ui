2a95dfae6e1f6ca3c9bde09566d8a6eba75c98be
Marcus Vinicius <marcusvy@gmail.com>
2015-01-05 23:09:37 -0400

    Fix Update: sass task

--- gulp/task/tool/sass.js
+++ gulp/task/tool/sass.js
@@ -6,13 +6,16 @@
 var mvApp = require('./../../index');
 var gulp = mvApp.gulp;
 var $ = mvApp.$();
+var dirDev = mvApp.config().dir().dev;
+var dirTmp = mvApp.config().dir().tmp;
 
 //== Node Sass
 gulp.task('tool:sass', function () {
   mvApp.$().browserSync.notify("Sass...");
+
   return gulp.src(dirDev + 'styles/**/*.scss')
     .pipe($.sass())
     .on('error', mvApp.error.handleError)
-    .pipe(gulp.dest(dirDev + 'styles/'))
+    .pipe(gulp.dest(dirTmp + 'styles/'))
     .pipe($.size());
 });
